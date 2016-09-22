5d269c5c822ead2e752c87f6c17db006ceb47b73
chlewsz <chlewsz@163.com>
2016-09-22 11:52:04 +0800

    遗漏提交

---
+++ PhotoChoose/MultiPhotoChooseViewController.m
@@ -0,0 +1,27 @@
+//
+//  MultiPhotoChooseViewController.m
+//  PhotoChoose
+//
+//  Created by wsz on 16/9/22.
+//  Copyright © 2016年 wsz. All rights reserved.
+//
+
+#import "MultiPhotoChooseViewController.h"
+
+@interface MultiPhotoChooseViewController ()
+
+@end
+
+@implementation MultiPhotoChooseViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.title = @"图片多选";
+}
+
+
+@end
