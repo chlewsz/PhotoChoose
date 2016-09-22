cd63187fcc5c7dd9be9c1fee296be3bc3726fd19
chlewsz <chlewsz@163.com>
2016-09-22 11:17:27 +0800

    Initial Commit

---
+++ PhotoChoose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoChoose
+//
+//  Created by wsz on 16/9/22.
+//  Copyright © 2016年 wsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
